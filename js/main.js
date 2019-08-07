c652a151d38fbeeab9894badb5049148d09bee6a
Mitchell Coleman Holmes <51342931+41Holmes41@users.noreply.github.com>
2019-08-07 04:52:05 -0500

    adds rounds feature and updates images and color scheme

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ let multiplier;
 let winnings;
 let totalWinnings;
 let pics;
+let individualCombos;
+let round;
+let lock;
+let targetedSpaces;
 
 /* ------initialize element variables -------*/
 
@@ -27,18 +31,32 @@ let image32 = document.querySelector('.option32-image');
 let image33 = document.querySelector('.option33-image');
 let wagerInput = document.querySelector('input');
 let wagerMessage = document.querySelector('.wager');
-let combo2 = [];
+let combo1 = document.querySelector('.combo1');
+let combo2 = document.querySelector('.combo2');
+let combo3 = document.querySelector('.combo3');
+let combo4 = document.querySelector('.combo4');
+let combo5 = document.querySelector('.combo5');
+let imageNodeList=  document.querySelectorAll("img");
+let spaceNodeList=  document.querySelectorAll(".space");
+
 
 
 
 
 /* ---------------------define functions---------------------- */
 function init() {
-    result=[];
+    result=[
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ];
     wager=0;
     multiplier=0;
     winnings=0;
     totalWinnings=50;
+    round = 0;
+    lock = 0;
+    targetedSpaces= [];
     pics = {
         1 : 'images\\skull-and-bones.png',
         2 : 'images\\diamond (6).png',
@@ -46,11 +64,32 @@ function init() {
         4 : 'images\\fashion (2).png',
         5 : 'images\\fashion.png'
     }
+    individualCombos = {
+        1 : 0,
+        2 : 0,
+        3 : 0,
+        4 : 0,
+        5 : 0
+    }
 
 /* ------------add event listeners------------------- */
     spin.addEventListener('click', function(){
-
-        result=[ [getResult(), getResult(), getResult()], [getResult(), getResult(), getResult()], [getResult(), getResult(), getResult()] ];
+        
+        let result2= [ result[0][0], result[0][1], result[0][2], result[1][0], result[1][1], result[1][2], result[2][0], result[2][1], result[2][2] ];
+        
+        function determination(number)  {
+            if ( (number !== targetedSpaces[0].dataset.square) && (number !== targetedSpaces[1].dataset.square) && (number !== targetedSpaces[2].dataset.square)){
+                return getResult();
+            } else {
+                return result2[number];
+            }
+        }
+        
+        result=[ 
+            [determination(1), determination(2), determination(3)], 
+            [determination(4), determination(5), determination(6)], 
+            [determination(7), determination(8), determination(9)] 
+        ];
     
         getCombo(result);
     
@@ -59,10 +98,30 @@ function init() {
         winnings = winnings +(wager * multiplier);
         totalWinnings += winnings;
         totalWinnings -= wager;
+        round += 1;
         
         render();
-        winnings=0;
-        multiplier = 0;
+        if (round>=3) {
+            
+            alert("Round Ended!  Click spin to begin new round!");
+            
+            resetBoard();
+            render();
+            round =0;
+            lock=0;
+        };
+
+    });
+
+    spaceNodeList.forEach(node=>{
+        node.addEventListener('click', function(evt){
+            if (lock < 3) {
+                targetedSpaces.push(evt.target);
+                //this.style.border = "15px solid black";
+                lock += 1;
+                render();
+            };
+        });
     });
 };
 
@@ -72,10 +131,18 @@ function render() {
     borderReset();
     placePics();
     renderBorders();
+    renderCombos();
+    console.log(lock);
+    if (round>=3) {
+        treasureReset();
+        borderReset();
+        
+    }
 
     wagerMessage.innerHTML=`<h3>Wager: $${wager} </h3>`;
     winningsMessage.innerHTML = `Winnings: $${winnings}`;
     totalWinningsMessage.innerHTML = `<h3>Total Winnings: $${totalWinnings}</h3>`;
+    spin.innerHTML= `Spin! Round ${round}`;
 
     
 }
@@ -83,8 +150,8 @@ function render() {
 function getResult() {
     let random = Math.random() * 100;
 
-    if ((random>=0)&&(random<=50)) return 1;
-    if ((random>50)&&(random<=70)) return 2;
+    if ((random>=0)&&(random<=45)) return 1;
+    if ((random>45)&&(random<=70)) return 2;
     if ((random>70)&&(random<=85)) return 3;
     if ((random>85)&&(random<=95)) return 4;
     if ((random>95)&&(random<=100)) return 5;
@@ -97,112 +164,65 @@ function getCombo(result) {
 
     if ( result[i][0] === result[i][1]) {
         if (result[i][1]===result[i][2]) {
-            if (result[i][0]===1) winnings -= wager;
-            if (result[i][0]===2) multiplier += 3;
-            if (result[i][0]===3) multiplier += 12;
-            if (result[i][0]===4) multiplier += 48;
-            if (result[i][0]===5) multiplier += 192;
-        } }/* else {
-        if (result[i][0]===1) multiplier += 0;
-        if (result[i][0]===2) multiplier += 1;
-        if (result[i][0]===3) multiplier += 4;
-        if (result[i][0]===4) multiplier += 8;
-        if (result[i][0]===5) multiplier += 16;
-        }
-    }
-    if (result[i][1] === result[i][2]) {
-        if (result[i][0]===1) multiplier += 0;
-        if (result[i][0]===2) multiplier += 1;
-        if (result[i][0]===4) multiplier += 4;
-        if (result[i][0]===3) multiplier += 8;
-        if (result[i][0]===5) multiplier += 16;
-    } */
+            if (result[i][0]===1) {multiplier -= 1; individualCombos[1]-=1;}
+            if (result[i][0]===2) {multiplier += 3; individualCombos[2]+=3;}
+            if (result[i][0]===3) {multiplier += 12; individualCombos[3]+=12;}
+            if (result[i][0]===4) {multiplier += 48; individualCombos[4]+=48;}
+            if (result[i][0]===5) {multiplier += 192; individualCombos[5]+=192;}
+        } }
         }
         
  for (var i=0; i<result.length; i++) {
     if ( result[0][i] === result[1][i]) {
         if (result[1][i]===result[2][i]) {
-            if (result[0][i]===1) winnings -= wager;
-            if (result[0][i]===2) multiplier += 3;
-            if (result[0][i]===3) multiplier += 12;
-            if (result[0][i]===4) multiplier += 48;
-            if (result[0][i]===5) multiplier += 192;
-        } }/* else {
-        if (result[0][i]===1) multiplier += 0;
-        if (result[0][i]===2) multiplier += 1;
-        if (result[0][i]===3) multiplier += 4;
-        if (result[0][i]===4) multiplier += 8;
-        if (result[0][i]===5) multiplier += 16;
-        }
-    }
-    if (result[1][i] === result[2][i]) {
-        if (result[0][i]===1) multiplier += 0;
-        if (result[0][i]===2) multiplier += 1;
-        if (result[0][i]===4) multiplier += 4;
-        if (result[0][i]===3) multiplier += 8;
-        if (result[0][i]===5) multiplier += 16;
-    } */
+            if (result[0][i]===1) {multiplier -= 1; individualCombos[1]-=1;}
+            if (result[0][i]===2) {multiplier += 3; individualCombos[2]+=3;}
+            if (result[0][i]===3) {multiplier += 12; individualCombos[3]+=12;}
+            if (result[0][i]===4) {multiplier += 48; individualCombos[4]+=48;}
+            if (result[0][i]===5) {multiplier += 192; individualCombos[5]+=192;}
+        } }
 }
 
  //--------------check for diagonal combos
  if (result[0][0]===result[1][1]) {
     if (result[1][1]===result[2][2]) {
-        if (result[0][1]===1) winnings -= wager;
-        if (result[0][1]===2) multiplier += 3;
-        if (result[0][1]===3) multiplier += 12;
-        if (result[0][1]===4) multiplier += 48;
-        if (result[0][1]===5) multiplier += 192;
-    } /* else {
-        if (result[0][1]===1) multiplier += 0;
-        if (result[0][1]===2) multiplier += 1;
-        if (result[0][1]===3) multiplier += 4;
-        if (result[0][1]===4) multiplier += 8;
-        if (result[0][1]===5) multiplier += 16;
-    }
-    if (result[1][1] === result[2][2]) {
-        if (result[1][1]===1) multiplier += 0;
-        if (result[1][1]===2) multiplier += 1;
-        if (result[1][1]===4) multiplier += 4;
-        if (result[1][1]===3) multiplier += 8;
-        if (result[1][1]===5) multiplier += 16;
-    } */
+        if (result[0][0]===1) {multiplier -= 1; individualCombos[1]-=1;}
+        if (result[0][0]===2) {multiplier += 3; individualCombos[2]+=3;}
+        if (result[0][0]===3) {multiplier += 12; individualCombos[3]+=12;}
+        if (result[0][0]===4) {multiplier += 48; individualCombos[4]+=48;}
+        if (result[0][0]===5) {multiplier += 192; individualCombos[5]+=192;}
+    } 
  }
  if (result[0][2]===result[1][1]) {
     if (result[1][1]===result[2][0]) {
-        if (result[0][2]===1) winnings -= wager;
-        if (result[0][2]===2) multiplier += 3;
-        if (result[0][2]===3) multiplier += 12;
-        if (result[0][2]===4) multiplier += 48;
-        if (result[0][2]===5) multiplier += 192;
-    } /* else {
-        if (result[0][2]===1) multiplier += 0;
-        if (result[0][2]===2) multiplier += 1;
-        if (result[0][2]===3) multiplier += 4;
-        if (result[0][2]===4) multiplier += 8;
-        if (result[0][2]===5) multiplier += 16;
-    }
-    if (result[1][1] === result[2][0]) {
-        if (result[1][1]===1) multiplier += 0;
-        if (result[1][1]===2) multiplier += 1;
-        if (result[1][1]===4) multiplier += 4;
-        if (result[1][1]===3) multiplier += 8;
-        if (result[1][1]===5) multiplier += 16;
-    } */
+        if (result[0][2]===1) {multiplier -= 1; individualCombos[1]-=1;}
+        if (result[0][2]===2) {multiplier += 3; individualCombos[2]+=3;}
+        if (result[0][2]===3) {multiplier += 12; individualCombos[3]+=12;}
+        if (result[0][2]===4) {multiplier += 48; individualCombos[4]+=48;}
+        if (result[0][2]===5){multiplier += 192; individualCombos[5]+=192;}
+    } 
  }
 
   //------------------------------------------------winning blackout combinations-------------------------------------------
-  if ((result[0][0]+result[0][1]+result[0][2]+result[1][0]+result[1][1]+result[1][2]+result[2][0]+result[2][1]+result[2][2])===(9*1)) {winnings -= wager;}
-  if ((result[0][0]+result[0][1]+result[0][2]+result[1][0]+result[1][1]+result[1][2]+result[2][0]+result[2][1]+result[2][2])===(9*2)) {winnings += wager;}
-  if ((result[0][0]+result[0][1]+result[0][2]+result[1][0]+result[1][1]+result[1][2]+result[2][0]+result[2][1]+result[2][2])===(9*3)) {winnings += (2*wager);}
-  if ((result[0][0]+result[0][1]+result[0][2]+result[1][0]+result[1][1]+result[1][2]+result[2][0]+result[2][1]+result[2][2])===(9*4)) {winnings += (4*wager);}
-  if ((result[0][0]+result[0][1]+result[0][2]+result[1][0]+result[1][1]+result[1][2]+result[2][0]+result[2][1]+result[2][2])===(9*5)) {winnings += (6*wager);}
+  if ( (result[0][0]===result[0][1]) && (result[0][1]===result[0][2]) && (result[0][2]===result[1][0]) && (result[1][0]===result[1][1]) && (result[1][1]===result[1][2]) && (result[1][2]===result[2][0]) && (result[2][0]===result[2][1]) && (result[2][1]===result[2][2]) ) {
+      if(result[0][0]===1) {multiplier -= 15; alert("Major Loss!!! Multiplier minus 15!");  individualCombos[1] -= 15}
+      if(result[0][0]===2) {multiplier += 30; alert("BIG TIME WIN!!! Multiplier plus 30!");  individualCombos[2] += 30}
+      if(result[0][0]===3) {multiplier += 300; alert("BIG TIME WIN!!! Multiplier plus 300!");  individualCombos[3] += 300}
+      if(result[0][0]===4) {multiplier += 3000; alert("BIG TIME WIN!!! Multiplier plus 3000!");  individualCombos[4] += 3000}
+      if(result[0][0]===5) {multiplier += 30000; alert("BIG TIME WIN!!! Multiplier plus 30000!");  individualCombos[5] += 30000}
+  }
 }
 
 //------------------------------this renders the boardersfor matches-------------------------------
 function renderBorders() {
-    let lossBorder = "10px solid red";
-    let bigWinBorder = "10px solid yellow";
+    let lossBorder = "15px solid #FF1654";
+    let bigWinBorder = "15px solid #247BA0";
     let smallWinBorder = "10px solid blue";
+    let targetBackground = "black";
+
+    for (let i=0; i < targetedSpaces.length; i++){
+        targetedSpaces[i].style.backgroundColor= targetBackground;
+       };
 
     if ((result[0][0]===result[0][1])&&(result[0][1]===result[0][2])) {
         if (result[0][0] === 1) {
@@ -268,8 +288,15 @@ function renderBorders() {
 
 //-----------------------------------Reset borders to NONE-----------------------------
 function borderReset(){
-    let nodelist=  document.querySelectorAll("img");
-    nodelist.forEach(node=>{node.style.border="none";})
+    imageNodeList.forEach(node=>{node.style.border="none";});
+    imageNodeList.forEach(node=>{node.style.backgroundColor="transparent";});
+
+};
+
+function treasureReset(){
+    spaceNodeList.forEach(node=>{
+        node.setAttribute('src', "images\\treasure.png")
+    });
 };
 
     //----------------------------------place pctures on board--------------------------------
@@ -292,5 +319,25 @@ function checkForLoss() {
     }
 }
 
+function renderCombos(){
+    combo1.innerHTML = `X${individualCombos[1]}`;
+    combo2.innerHTML = `X${individualCombos[2]}`;
+    combo3.innerHTML = `X${individualCombos[3]}`;
+    combo4.innerHTML = `X${individualCombos[4]}`;
+    combo5.innerHTML = `X${individualCombos[5]}`;
+};
+
+function resetBoard() {
+    winnings=0;
+    multiplier = 0;
+    individualCombos[1]=0;
+    individualCombos[2]=0;
+    individualCombos[3]=0;
+    individualCombos[4]=0;
+    individualCombos[5]=0;
+    lock=0;
+    targetedSpaces=[];
+    
+};
 
 init();
